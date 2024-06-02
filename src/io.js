a4d0dd6efa564e281ae1588e3c7f2aca4c8ab416
fruitbox12 <dylanwong007@gmail.com>
2024-06-02 01:09:31 -0700

    Update io.js

--- src/io.js
+++ src/io.js
@@ -1,23 +1,14 @@
-const readline = require('readline');
+const readlineSync = require('readline-sync');
 const { Keyboard } = require('./utils');
 
 class IO {
     constructor() {
         this.keyboard = new Keyboard();
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
     }
 
     setup(world) {
         this.world = world;
         this.keyboard.setup();
-        this.rl.on('line', (input) => {
-            if (input === 'q') {
-                process.exit(0);
-            }
-        });
     }
 
     update() {
@@ -28,6 +19,20 @@ class IO {
                 console.log(line);
             });
         });
+
+        // Handle user input
+        if (this.keyboard.isPressed('d')) {
+            this.world.sprites.forEach(sprite => {
+                sprite.x += 1;
+                if (sprite.x > 80) sprite.x = 0;
+            });
+        }
+        if (this.keyboard.isPressed('a')) {
+            this.world.sprites.forEach(sprite => {
+                sprite.x -= 1;
+                if (sprite.x < 0) sprite.x = 80;
+            });
+        }
     }
 }
 
