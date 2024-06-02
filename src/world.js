50eeea50ec4f361b7161572c77a44c8d9f3d78a3
fruitbox12 <dylanwong007@gmail.com>
2024-06-02 01:23:33 -0700

    Update world.js

--- src/world.js
+++ src/world.js
@@ -1,41 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const Map = require('./Map');
+const Minimap = require('./Minimap');
+const Unit = require('./Unit');
+
 class World {
-    constructor() {
-        this.sprites = [];
-        this.spriteData = {};
-        this.width = 80;
-        this.height = 24;
-    }
+  static async create() {
+    const world = new World();
+    await world.loadAssets();
+    world.init();
+    return world;
+  }
 
-    addSpriteData(name, data) {
-        this.spriteData[name] = data;
+  async loadAssets() {
+    this.sprites = {};
+    const files = fs.readdirSync(path.join(__dirname, '../assets'));
+    for (const file of files) {
+      const data = fs.readFileSync(path.join(__dirname, '../assets', file), 'utf8');
+      this.sprites[file] = data;
     }
+  }
 
-    setup() {
-        // Parse the sprite data and create text-based sprites
-        Object.keys(this.spriteData).forEach(name => {
-            const data = this.spriteData[name];
-            const sprite = this.createSpriteFromData(name, data);
-            this.sprites.push(sprite);
-        });
-    }
+  init() {
+    this.map = new Map();
+    this.minimap = new Minimap();
+    this.units = [];
+    this.loadUnits();
+    // Additional initialization logic
+  }
 
-    createSpriteFromData(name, data) {
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-        return {
-            name,
-            data: lines,
-            x: Math.floor(Math.random() * this.width),
-            y: Math.floor(Math.random() * this.height)
-        };
-    }
+  loadUnits() {
+    // Load initial units for the game
+    this.units.push(new Unit('probe', { x: 1, y: 1 }));
+  }
 
-    update() {
-        // Update the world state
-        this.sprites.forEach(sprite => {
-            sprite.x += 1;
-            if (sprite.x > this.width) sprite.x = 0;
-        });
-    }
+  executeCommand(command) {
+    // Parse and execute the command
+    console.log(`Executing command: ${command}`);
+  }
+
+  update() {
+    // Update world state
+    console.log('Updating world state');
+  }
 }
 
-module.exports = { World };
+module.exports = World;
