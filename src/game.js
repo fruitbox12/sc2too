a1f1246df29c65cba29d8614f2a6a798df28e743
fruitbox12 <dylanwong007@gmail.com>
2024-06-02 00:59:17 -0700

    Update game.js

--- src/game.js
+++ src/game.js
@@ -16,9 +16,26 @@ class Game {
     }
 
     loadAssets() {
-        PIXI.Loader.shared
-            .add('spritesheet', 'data/sprites/spritesheet.json')
-            .load(() => this.setup());
+        const spriteFiles = [
+            'assimilator.txt', 'assimilator_empty.txt', 'boost3x3.txt', 'boost5x5.txt',
+            'building2x2.txt', 'building3x3.txt', 'building5x5.txt', 'cyber_core.txt',
+            'cyber_core_researching.txt', 'gateway.txt', 'gateway_producing.txt', 'geyser.txt',
+            'geyser_empty.txt', 'mineral.txt', 'nexus.txt', 'nexus_producing.txt', 'probe.txt',
+            'probe_gas.txt', 'probe_gather.txt', 'probe_mineral.txt', 'pylon.txt', 'rock.txt',
+            'rock2x2.txt', 'stalker.txt', 'zealot.txt'
+        ];
+
+        let loadCount = 0;
+        spriteFiles.forEach(file => {
+            fetch(`data/sprites/${file}`)
+                .then(response => response.text())
+                .then(text => {
+                    this.world.addSpriteData(file, text);
+                    if (++loadCount === spriteFiles.length) {
+                        this.setup();
+                    }
+                });
+        });
     }
 
     setup() {
