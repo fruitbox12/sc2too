c552de316123912f39fcecac98c56713394d779f
fruitbox12 <dylanwong007@gmail.com>
2024-06-02 01:12:27 -0700

    Update io.js

--- src/io.js
+++ src/io.js
@@ -13,26 +13,35 @@ class IO {
 
     update() {
         console.clear();
-        this.world.sprites.forEach(sprite => {
-            console.log(`Sprite ${sprite.name} at (${sprite.x}, ${sprite.y}):`);
-            sprite.data.forEach(line => {
-                console.log(line);
-            });
-        });
+        this.renderWorld();
 
         // Handle user input
         if (this.keyboard.isPressed('d')) {
             this.world.sprites.forEach(sprite => {
                 sprite.x += 1;
-                if (sprite.x > 80) sprite.x = 0;
+                if (sprite.x > this.world.width) sprite.x = 0;
             });
         }
         if (this.keyboard.isPressed('a')) {
             this.world.sprites.forEach(sprite => {
                 sprite.x -= 1;
-                if (sprite.x < 0) sprite.x = 80;
+                if (sprite.x < 0) sprite.x = this.world.width;
             });
         }
+    }
+
+    renderWorld() {
+        const screen = Array(this.world.height).fill('').map(() => Array(this.world.width).fill(' '));
+        this.world.sprites.forEach(sprite => {
+            sprite.data.forEach((line, y) => {
+                for (let x = 0; x < line.length; x++) {
+                    if (sprite.y + y < this.world.height && sprite.x + x < this.world.width) {
+                        screen[sprite.y + y][sprite.x + x] = line[x];
+                    }
+                }
+            });
+        });
+        console.log(screen.map(line => line.join('')).join('\n'));
     }
 }
 
